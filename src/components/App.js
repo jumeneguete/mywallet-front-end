a49b9a37f2fd5cdf9466c17cf4e385e131e9a220
Juliana Meneguete <jumeneguete@gmail.com>
2021-06-24 13:55:30 -0300

    Login e UserContext

--- src/components/App.js
+++ src/components/App.js
@@ -5,28 +5,34 @@ import SignUp from './SignUp'
 import Home from './Home'
 import CashIn from './CashIn'
 import CashOut from './CashOut'
+import { useState } from "react";
+import UserContext from "./contexts/UserContext";
 
 export default function App() {
+    const [user, setUser] = useState("");
+
     return (
         <>
-        <GlobalStyle />
+            <GlobalStyle />
             <BrowserRouter>
                 <Switch>
-                    <Route path="/" exact>
-                        <Login />
-                    </Route>
-                    <Route path="/signup" exact>
-                        <SignUp />
-                    </Route>
-                    <Route path="/home" exact>
-                        <Home />
-                    </Route>
-                    <Route path="/cashin" exact>
-                        <CashIn />
-                    </Route>
-                    <Route path="/cashout" exact>
-                        <CashOut />
-                    </Route>
+                    <UserContext.Provider value={{ user, setUser }}>
+                        <Route path="/" exact>
+                            <Login />
+                        </Route>
+                        <Route path="/signup" exact>
+                            <SignUp />
+                        </Route>
+                        <Route path="/home" exact>
+                            <Home />
+                        </Route>
+                        <Route path="/cashin" exact>
+                            <CashIn />
+                        </Route>
+                        <Route path="/cashout" exact>
+                            <CashOut />
+                        </Route>
+                    </UserContext.Provider>
                 </Switch>
             </BrowserRouter>
         </>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,48 @@
 import Input from "./Input";
 import Button from "./Button";
 import { Main, Logo } from "./Styles";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { useState, useContext } from "react";
+import axios from "axios";
+import UserContext from "./contexts/UserContext";
+import ThreeDots from "./ThreeDots";
 
 export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const {setUser} = useContext(UserContext);
+    const history = useHistory();
+
+    function login(e){
+        e.preventDefault();
+
+        if (!email || !password) return alert("Preencha todos os campos!");
+
+        const body = {email, password}
+        const request = axios.post("http://localhost:4000/", body);
+        setLoading(true);
+        request.then(response => {
+            console.log(response.data);
+            setUser(response.data)
+            history.push("/home");
+        })
+
+        request.catch(() => {
+            alert("E-mail ou senha incorretos!");
+            setLoading(false);
+        });
+
+
+    }
+
     return (
         <Main>
             <Logo>MyWallet</Logo>
-            <form>
-                <Input type="email" placeholder="E-mail" />
-                <Input type="password" placeholder="Senha" />
-                <Button type="submit">Entrar</Button>
+            <form onSubmit={login}>
+                <Input type="email" value={email} setValue={setEmail} disabled={loading} placeholder="E-mail" />
+                <Input type="password" value={password} setValue={setPassword} disabled={loading} placeholder="Senha" />
+                <Button type="submit">{loading ? <ThreeDots /> : "Entrar"}</Button>
             </form>
             <Link to="/signup"><p>Primeira vez? Cadastre-se!</p></Link>
         </Main>
